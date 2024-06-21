f80d396cf063b1b9b507518aa1a6debb527cfad5
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-06-21 10:39:05 -0400

    Feat: add basic operatios
    - sum, sub, mult, div

---
+++ script.js
@@ -0,0 +1,32 @@
+let op, n1, n2;
+
+
+function operate(userOp, n1, n2){
+  const OPERATIONS = ['+', '-', '*', '/']
+  switch (OPERATIONS.findIndex(op => op === userOp)) {
+    case 0:
+      return sum(n1 ,n2);
+    case 1:
+      return sub(n1 ,n2);
+    case 2:
+      return mult(n1 ,n2);
+    case 3:
+      return div(n1 ,n2);
+  }
+}
+
+function sum(a, b){
+  return a + b;
+}
+
+function sub(a, b){
+  return a - b;
+}
+
+function mult(a, b){
+  return a * b;
+}
+
+function div(a, b){
+  return b === 0 ? Infinity : a / b; 
+}
