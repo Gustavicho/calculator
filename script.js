6df75be90357150547860516af1f91ab67e8b877
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-06-21 15:08:33 -0400

    Fix: can't add multple decimal points

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 let op, n1, n2;
 const display = document.querySelector('.result');
 
+// all nums buttons
 document.querySelectorAll('.num-btn').forEach(btn => {
   btn.addEventListener('click', e => {
     display.textContent += e.target.value; 
   });
 });
 
+// clear button
 document.querySelector('.clear-btn').addEventListener('click', clear)
+// decimal point button
+document.querySelector('.decimal-btn').addEventListener('click', () => {
+  const actualNum = display.textContent.split('');
+  // make the num have ONLY 1 decimal point
+  if (!actualNum.includes('.'))
+    display.textContent += '.';
+});
 
 function operate(userOp, n1, n2){
   const OPERATIONS = ['+', '-', '*', '/']
