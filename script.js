c7aeea53ffa68a05b01b1cda3c4d66b4888cf8ed
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-06-21 16:29:54 -0400

    Feat: the calculator works

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 let op, n1, n2;
+let isOpDefined   = false;    
+let isNum1Defined = false;
+let isNum2Defined = false;
+let temp = false;
 const display = document.querySelector('.result');
 
-// all nums buttons
+// all numbers buttons
 document.querySelectorAll('.num-btn').forEach(btn => {
   btn.addEventListener('click', e => {
-    display.textContent += e.target.value; 
+    if (temp){
+      temp = false;
+      display.textContent = '';  
+    }
+    display.textContent += e.target.value;
   });
 });
-
+// all operations buttons
+document.querySelectorAll('.op-btn').forEach(btn => {
+  btn.addEventListener('click', e => {
+    getNumber();
+    if(isNum1Defined && isNum2Defined && isOpDefined){
+      equals()
+    }
+    temp = true;
+    isOpDefined = true;
+    op = e.target.value;
+  });
+});
+// eqauls button
+document.querySelector('.equals-btn').addEventListener('click', () => {
+  temp = true;
+  getNumber();
+  equals()
+});
 // clear button
-document.querySelector('.clear-btn').addEventListener('click', clear)
+document.querySelector('.clear-btn').addEventListener('click', clear);
 // decimal point button
 document.querySelector('.decimal-btn').addEventListener('click', () => {
   const actualNum = display.textContent.split('');
@@ -32,7 +57,28 @@ function operate(userOp, n1, n2){
   }
 }
 
+function equals(){
+  const result = operate(op, n1, n2);
+  display.textContent = result;
+  n1 = result;
+  isNum2Defined = false;
+  isOpDefined   = false;
+}
+
+function getNumber(){
+  if (!isNum1Defined){
+    isNum1Defined = true;
+    n1 = +display.textContent;
+  } else if (!isNum2Defined){
+    isNum2Defined = true;
+    n2 = +display.textContent;
+  }
+}
+
 function clear(){
+  op = undefined; isOpDefined   = false;
+  n1 = undefined; isNum1Defined = false;
+  n2 = undefined; isNum2Defined = false;
   display.textContent = '';
 }
 
